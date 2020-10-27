7411ff6b49aff21cbd4395ca84b1fe04c6e9396c
Casey Escovedo <caseyjescovedo@gmail.com>
2020-10-27 16:51:39 -0700

    Working get request to API

--- server/mapController.js
+++ server/mapController.js
@@ -25,17 +25,28 @@ const conversion = {
   '2': 90
 }
 
+const latlong = {
+  switzerFalls: { latitude: 34.2638892 , longitude: -118.1740902 },
+  griffithPark: { latitude: 34.1281475 , longitude: -118.3010914 },
+  elysianPark: { latitude: 34.0820739 , longitude: -118.2497133 },
+  eatonCanyon: { latitude: 34.1783564 , longitude: -118.0966051 },
+  runyonCanyon: { latitude: 34.1193155,  longitude: -118.353079 }
+}
+
 mapController.getHeat = (req, res, next) => {
   //deconstruct request body to get desired location
-
+  // const { trail } = req.body;
+  const trail = 'griffithPark';
   //make fetch call to API
-  fetch(`https://besttime.app/api/v1/forecasts/now?api_key_public=${publicKey}&venue_id=${locations.griffithPark}`)
+  fetch(`https://besttime.app/api/v1/forecasts/now?api_key_public=${publicKey}&venue_id=${locations[trail]}`)
     .then(data => {
       data.json()
         .then(parsedData => {
           //the returned intensity variable rates traffic on a scale from -2 to 2, and 999 indicates the trail is closed
-          const intensity = conversion[parsedData.analysis.hour_analysis.intensity_nr];
-          res.locals.data = intensity;
+          const weight = conversion[parsedData.analysis.hour_analysis.intensity_nr];
+      
+          res.locals.data = { latitude: latlong[trail].latitude, longitude: latlong[trail].longitude, weight};
+          console.log(data);
           return next();
         })
     })
