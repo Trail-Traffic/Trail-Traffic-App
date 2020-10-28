e617f03a5b5fc40b89f2948df9aa71896a9735df
Amy Yee <amyyee1998@gmail.com>
2020-10-28 13:17:07 -0400

    Fixed merge issues

--- screens/SplashLoginPage.jsx
+++ screens/SplashLoginPage.jsx
@@ -1,14 +1,1 @@
-<<<<<<< HEAD
 // import { StyleSheet, Text, View } from "react-native";
-=======
-// import React from "react";
-// import { View, Text } from 'react-native';
-
-// export default function Splash() {
-//     return (
-//         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-//         <Text>Splash page</Text>
-//     </View>
-//     );
-// }
->>>>>>> main
