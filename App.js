2cbef9dfae291f3be58801c53d8d77c17e298529
Amy Yee <amyyee1998@gmail.com>
2020-10-28 20:27:50 -0400

    Moved map page over

--- App.js
+++ App.js
@@ -18,6 +18,8 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import Icon from "react-native-vector-icons/FontAwesome";
+import {MapPage} from './screens/MapPage.jsx'
+
 // import {
 //   GoogleSignin,
 //   GoogleSigninButton,
@@ -25,62 +27,6 @@ import Icon from "react-native-vector-icons/FontAwesome";
 // } from "@react-native-community/google-signin";
 
 // GoogleSignin.configure();
-
-//========================================= MAP PAGE ================================================//
-function MapPage() {
-  const [trails, setTrails] = useState([]);
-
-  useEffect(() => {
-    fetch("http://192.168.0.197:5001/api/getData")
-      .then((res) => res.json())
-      .then((res) => setTrails(res))
-      .catch((err) => console.log(err));
-  }, []);
-
-  return (
-    <View style={styles.container}>
-      <MapView
-        initialRegion={{
-          latitude: 34.052235,
-          longitude: -118.243683,
-          latitudeDelta: 0.09,
-          longitudeDelta: 0.0121,
-        }}
-        provider={PROVIDER_GOOGLE}
-        style={{ flex: 1 }}
-      >
-        {trails.map((marker, i) => (
-          <Marker
-            key={i}
-            coordinate={{
-              latitude: marker.latitude,
-              longitude: marker.longitude,
-            }}
-          >
-            <Callout tooltip>
-              <View>
-                <View style={styles.bubble}>
-                  <Text style={styles.name}>Test Hello</Text>
-                </View>
-                <View style={styles.arrowBorder} />
-                <View style={styles.arrow} />
-              </View>
-            </Callout>
-            </Marker>
-        ))}
-        <Heatmap
-          points={trails}
-          radius={50}
-          gradient={{
-            colors: ["#0DE5FF", "#0D14FF", "#980DFF", "#FF0DED", "#E50000"],
-            startPoints: [0.01, 0.25, 0.5, 0.75, 1],
-            colorMapSize: 256,
-          }}
-        />
-      </MapView>
-    </View>
-  );
-}
 
 //========================================= SPLASH/LOGIN PAGE ================================================//
 
@@ -211,43 +157,6 @@ const styles = StyleSheet.create({
   map: {
     flex: 1,
   },
-  bubble: {
-    flexDirection: 'column',
-    alignSelf: 'flex-start',
-    backgroundColor: '#fff',
-    borderRadius: 6,
-    borderColor: '#ccc',
-    borderWidth: 0.5,
-    padding: 15,
-    width: 150,
-  },
-  // Arrow below the bubble
-  arrow: {
-    backgroundColor: 'transparent',
-    borderColor: 'transparent',
-    borderTopColor: '#fff',
-    borderWidth: 16,
-    alignSelf: 'center',
-    marginTop: -32,
-  },
-  arrowBorder: {
-    backgroundColor: 'transparent',
-    borderColor: 'transparent',
-    borderTopColor: '#007a87',
-    borderWidth: 16,
-    alignSelf: 'center',
-    marginTop: -0.5,
-    // marginBottom: -15
-  },
-  // Character name
-  name: {
-    fontSize: 16,
-    marginBottom: 5,
-  },
-  // Character image
-  image: {
-    width: "100%",
-    height: 80,
   googleButton: {
     flexDirection: "row",
     alignItems: "center",
--- screens/MapPage.jsx
+++ screens/MapPage.jsx
@@ -1,3 +1,356 @@
-// import React from "react";
-// import MapView from "react-native-maps";
-// import * as Location from "expo-location";
+import React, { useEffect, useState } from "react";
+import MapView, {
+  PROVIDER_GOOGLE,
+  Heatmap,
+  Marker,
+  Callout,
+} from "react-native-maps";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Image,
+  Alert,
+  Modal,
+  TouchableHighlight,
+  TouchableOpacity,
+} from "react-native";
+
+export function MapPage() {
+  const [trails, setTrails] = useState([]);
+
+  useEffect(() => {
+    fetch("http://192.168.0.197:5001/api/getData")
+      .then((res) => res.json())
+      .then((res) => setTrails(res))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <MapView
+        initialRegion={{
+          latitude: 34.052235,
+          longitude: -118.243683,
+          latitudeDelta: 0.09,
+          longitudeDelta: 0.0121,
+        }}
+        provider={PROVIDER_GOOGLE}
+        style={{ flex: 1 }}
+      >
+        {trails.map((marker, i) => (
+          <Marker
+            key={i}
+            coordinate={{
+              latitude: marker.latitude,
+              longitude: marker.longitude,
+            }}
+          >
+            <Callout tooltip>
+              <View>
+                <View style={styles.bubble}>
+                  <Text style={styles.name}>Test Hello</Text>
+                </View>
+                <View style={styles.arrowBorder} />
+                <View style={styles.arrow} />
+              </View>
+            </Callout>
+          </Marker>
+        ))}
+        <Heatmap
+          points={trails}
+          radius={50}
+          gradient={{
+            colors: ["#0DE5FF", "#0D14FF", "#980DFF", "#FF0DED", "#E50000"],
+            startPoints: [0.01, 0.25, 0.5, 0.75, 1],
+            colorMapSize: 256,
+          }}
+        />
+      </MapView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "red",
+  },
+  map: {
+    flex: 1,
+  },
+  // Callout bubble
+  bubble: {
+    flexDirection: "column",
+    alignSelf: "flex-start",
+    backgroundColor: "#fff",
+    borderRadius: 6,
+    borderColor: "#ccc",
+    borderWidth: 0.5,
+    padding: 15,
+    width: 150,
+  },
+  // Arrow below the bubble
+  arrow: {
+    backgroundColor: "transparent",
+    borderColor: "transparent",
+    borderTopColor: "#fff",
+    borderWidth: 16,
+    alignSelf: "center",
+    marginTop: -32,
+  },
+  arrowBorder: {
+    backgroundColor: "transparent",
+    borderColor: "transparent",
+    borderTopColor: "#007a87",
+    borderWidth: 16,
+    alignSelf: "center",
+    marginTop: -0.5,
+    // marginBottom: -15
+  },
+  // Character name
+  name: {
+    fontSize: 16,
+    marginBottom: 5,
+  },
+  // Character image
+  image: {
+    width: "100%",
+    height: 80,
+  },
+});
+
+const mapDarkMode = [
+  {
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#1d2c4d",
+      },
+    ],
+  },
+  {
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#8ec3b9",
+      },
+    ],
+  },
+  {
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#1a3646",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.country",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#4b6878",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.land_parcel",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#64779e",
+      },
+    ],
+  },
+  {
+    featureType: "administrative.province",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#4b6878",
+      },
+    ],
+  },
+  {
+    featureType: "landscape.man_made",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#334e87",
+      },
+    ],
+  },
+  {
+    featureType: "landscape.natural",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#023e58",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#283d6a",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#6f9ba5",
+      },
+    ],
+  },
+  {
+    featureType: "poi",
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#1d2c4d",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#023e58",
+      },
+    ],
+  },
+  {
+    featureType: "poi.park",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#3C7680",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#304a7d",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#98a5be",
+      },
+    ],
+  },
+  {
+    featureType: "road",
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#1d2c4d",
+      },
+    ],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#2c6675",
+      },
+    ],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "geometry.stroke",
+    stylers: [
+      {
+        color: "#255763",
+      },
+    ],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#b0d5ce",
+      },
+    ],
+  },
+  {
+    featureType: "road.highway",
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#023e58",
+      },
+    ],
+  },
+  {
+    featureType: "transit",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#98a5be",
+      },
+    ],
+  },
+  {
+    featureType: "transit",
+    elementType: "labels.text.stroke",
+    stylers: [
+      {
+        color: "#1d2c4d",
+      },
+    ],
+  },
+  {
+    featureType: "transit.line",
+    elementType: "geometry.fill",
+    stylers: [
+      {
+        color: "#283d6a",
+      },
+    ],
+  },
+  {
+    featureType: "transit.station",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#3a4762",
+      },
+    ],
+  },
+  {
+    featureType: "water",
+    elementType: "geometry",
+    stylers: [
+      {
+        color: "#0e1626",
+      },
+    ],
+  },
+  {
+    featureType: "water",
+    elementType: "labels.text.fill",
+    stylers: [
+      {
+        color: "#4e6d70",
+      },
+    ],
+  },
+];
