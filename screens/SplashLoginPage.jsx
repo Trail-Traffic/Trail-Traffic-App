74f5bff30c48fa58f9fbb51dece9465dce86512b
Casey Walker <casey.e.walker@gmail.com>
2020-10-28 19:26:25 -0700

    google ouath set up on splash page
    Co-authored-by: Casey Escovedo caseyjescovedo@gmail.com

--- screens/SplashLoginPage.jsx
+++ screens/SplashLoginPage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { Component, useEffect, useState } from "react";
 import {
   StyleSheet,
   Text,
@@ -11,24 +11,55 @@ import {
   TouchableOpacity,
   ScrollView,
   SafeAreaView,
+  TouchableWithoutFeedback
 } from "react-native";
 import { Card, ListItem } from "react-native-elements";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import Icon from "react-native-vector-icons/FontAwesome";
+import * as Google from "expo-google-app-auth";
+import secret from "../secrets"
+
+const IOS_CLIENT_ID = secret.google_client_id;
 
 export function Splash({ navigation }) {
-    return (
+
+  signInWithGoogle = async () => {
+    try {
+      const result = await Google.logInAsync({
+        iosClientId: IOS_CLIENT_ID,
+        // androidClientId: ANDROID_CLIENT_ID,
+        scopes: ["profile", "email"]
+      });
+
+      if (result.type === "success") {
+        console.log("LoginScreen.js.js 21 | ", result.user.givenName);
+        this.props.navigation.navigate("Profile", {
+          username: result.user.givenName
+        }); //after Google login redirect to Profile
+        return result.accessToken;
+      } else {
+        return { cancelled: true };
+      }
+    } catch (e) {
+      console.log('LoginScreen.js.js 30 | Error with login', e);
+      return { error: true };
+    }
+  };
+
+  return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
         <Image source={require("../assets/logo.png")} />
-        <Icon.Button name="facebook" backgroundColor="#3b5998">
+        <Icon.Button name="facebook" backgroundColor="#3b5998" onPress={() => console.log('clicked facebook')}>
           Login with Facebook
         </Icon.Button>
         <TouchableOpacity>
-          <View style={styles.googleButton}>
-            <Image source={require("../assets/google2.png")}></Image>
-          </View>
+          <TouchableWithoutFeedback onPress={signInWithGoogle}>
+            <View style={styles.googleButton}>
+              <Image source={require("../assets/google2.png")}></Image>
+            </View>
+          </TouchableWithoutFeedback>
         </TouchableOpacity>
         {/* <GoogleSigninButton
           style={{ width: 192, height: 48 }}
